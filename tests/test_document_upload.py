2f21a248f66e8c5e04f3234f9ecf4cde317fe93d
Paddy Moran <pdmoran@outlook.com>
2017-05-11 16:25:06 +1200

    Fix tests

--- tests/test_document_upload.py
+++ tests/test_document_upload.py
@@ -1,8 +1,12 @@
-from db import  upsert_user, add_document, find_or_create_and_validate_document_set, get_document, get_set_info
+from db import (
+    upsert_user, add_document, get_document, get_set_info,
+    find_or_create_and_validate_document_set
+)
 from tests import DBTestCase
-import uuid
+from uuid import uuid4
 import server
 import os
+
 
 class TestDocumentUpload(DBTestCase):
 
@@ -14,14 +18,16 @@ class TestDocumentUpload(DBTestCase):
                         'name': 'testuser',
                         'email': 'testuser@email.com'
                         })
-            set_id = str(uuid.uuid4())
+            set_id = str(uuid4())
+            document_id = str(uuid4())
             find_or_create_and_validate_document_set(set_id, USER_ID)
             with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fixtures/pdfs/form-pdf.pdf')) as f:
                 binary_data = f.read()
-                result = add_document(set_id, 'filename', binary_data)
+                result = add_document(set_id, document_id, 'filename', binary_data)
 
             document_info = get_document(USER_ID, result['document_id'])
-            self.assertEqual('dbe5c4a1c0f4d8bd595b4465a81dd4b4adbf16685fd46c5668761b73ecb18de0', document_info['hash'])
+            expected_hash = "dbe5c4a1c0f4d8bd595b4465a81dd4b4adbf16685fd46c5668761b73ecb18de0"
+            self.assertEqual(expected_hash, document_info['hash'])
             self.assertEqual(binary_data, str(document_info['data']))
 
             set_info = get_set_info(USER_ID, set_id)

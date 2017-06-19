f7248981c2efdd124393590ffa74a3d7aa09c146
Paddy Moran <pdmoran@outlook.com>
2017-06-19 16:22:17 +1200

    pep8 the seed file

--- seed.py
+++ seed.py
@@ -1,11 +1,19 @@
+"""
+
+Seed file
+
+"""
 from __future__ import print_function
-import db
+from db import upsert_user
 
 def run():
+    """
+    Seed the database
+    """
     upsert_user({
-                'name': 'signatureuser',
-                'email': 'signatureuser@email.com'
-                })
+        'name': 'signatureuser',
+        'email': 'signatureuser@email.com'
+    })
 
 if __name__ == '__main__':
     run()

588adc2173cfed8dbd45a1d80528b26afe75ba9d
Paddy Moran <pdmoran@outlook.com>
2017-08-03 15:55:56 +1200

    Open signed pdf in new window

--- src/js/sagas/documentViewerSagas.ts
+++ src/js/sagas/documentViewerSagas.ts
@@ -13,7 +13,7 @@ function *signDocumentSaga() {
         try {
             const response = yield call(axios.post, '/api/sign', action.payload);
 
-            const signedPDFLink = window.location.origin + '/signed-documents/' + response.data.file_id + '?filename=test.pdf';
+            const signedPDFLink = window.location.origin + '/document/' + response.data.document_id;
             window.open(signedPDFLink, '_blank');
 
             yield put(setSignRequestStatus(Sign.DownloadStatus.Complete));

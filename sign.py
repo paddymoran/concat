e329885798cce56b91e922a96527c592d9625fdd
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-08-03 15:57:09 +1200

    drawing all signatures at once per page

--- sign.py
+++ sign.py
@@ -1,31 +1,33 @@
-import sys
-import os
 from pdfrw import PdfReader, PdfWriter, PageMerge
 from reportlab.pdfgen import canvas
-from pdfrw.buildxobj import pagexobj
 from io import BytesIO
 from reportlab.lib.utils import ImageReader
-from io import BytesIO
+from collections import defaultdict
 # Get all the filenames
 
 def sign(input_file, signatures):
 
     pdf = PdfReader(input_file)
     print(signatures)
+    # group by page
+    page_map = defaultdict(list)
     for signature in signatures:
-        image = ImageReader(signature['signature'])
-
-        page = pdf.pages[signature['pageNumber'] -1 ]
+        page_map[signature['pageNumber']].append(signature)
+    for page_number, signatures in page_map.items():
+        page = pdf.pages[page_number]
         mbox = tuple(float(x) for x in page.MediaBox)
         signature_data = BytesIO()
         signature_page = canvas.Canvas(signature_data, tuple(mbox[2:]))
-        signature_page.drawImage(image, mbox[2] * signature['offsetX'], mbox[3] - (mbox[3] * signature['offsetY']), mbox[2] * signature['ratioX'], mbox[3] * signature['ratioY'], mask='auto')
+        for signature in signatures:
+            print('adding')
+            image = ImageReader(signature['signature'])
+            signature_page.drawImage(image, mbox[2] * signature['offsetX'], mbox[3] - (mbox[3] * signature['offsetY']), mbox[2] * signature['ratioX'], mbox[3] * signature['ratioY'], mask='auto')
         signature_page.save()
         signature_data.seek(0)
-        PageMerge(page).add(PdfReader(signature_data).pages[0]).render()
-        print(mbox)
 
-    out = BytesIO();
+        PageMerge(page).add(PdfReader(signature_data).pages[0]).render()
+
+    out = BytesIO()
     PdfWriter(out, trailer=pdf).write()
     out.seek(0)
     return out

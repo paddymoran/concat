837c950b307436ab361d21c7dbbdca53ac440c72
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-08-09 16:15:36 +1200

    probably fixed delete signature

--- src/js/reducers/signatures.ts
+++ src/js/reducers/signatures.ts
@@ -9,7 +9,8 @@ export default function signatures(state: Sign.Signatures = DEFAULT_STATE, actio
 
         case Sign.Actions.Types.SET_SIGNATURE_IDS:
             return setSignatureIds(state, action);
-
+        case Sign.Actions.Types.DELETE_SIGNATURE:
+            return deleteSignature(state, action);
         default:
             return state;
     }
@@ -29,4 +30,12 @@ function setSignatureIds(state: Sign.Signatures, action: Sign.Actions.SetSignatu
         signatureIds: action.payload.signatureIds,
         initialIds: action.payload.initialIds
     };
-}
+
+
+
+function deleteSignature(state: Sign.Signatures, action: Sign.Actions.DeleteSignature): Sign.Signatures {
+    return {
+        ...state,
+        signatureIds: state.signatureIds.filter(id => id !== action.payload.signatureId),
+        initialIds: state.initialIds.filter(id => id !== action.payload.signatureId)
+    };

98afed945e7b068a7cabdcfdec0ac878476b83e9
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-09-06 16:43:46 +1200

    bolding active links of document page

--- src/js/components/documents.tsx
+++ src/js/components/documents.tsx
@@ -9,10 +9,10 @@ export default class Documents extends React.PureComponent {
 
                 <div className="center-nav-pills">
                     <ul className="nav nav-pills">
-                        <li><Link to="/all">All</Link></li>
-                        <li><Link to="/to_sign">To Sign</Link></li>
-                        <li><Link to="/pending">Pending</Link></li>
-                        <li><Link to="/completed">Completed</Link></li>
+                        <li><Link to="/all" activeClassName="active">All</Link></li>
+                        <li><Link to="/to_sign"  activeClassName="active">To Sign</Link></li>
+                        <li><Link to="/pending"  activeClassName="active">Pending</Link></li>
+                        <li><Link to="/completed"  activeClassName="active">Completed</Link></li>
                     </ul>
                 </div>
 

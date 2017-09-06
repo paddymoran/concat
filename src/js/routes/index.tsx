4e43172c41a56d6d833f4526e02f45682689d7de
Paddy Moran <pdmoran@outlook.com>
2017-09-07 10:57:15 +1200

    Remove faw

--- src/js/routes/index.tsx
+++ src/js/routes/index.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import { IndexRoute, Route } from 'react-router';
-import App, { ContainerWithStatusBar, Container } from '../components/app';
+import App, { ContainerWithStatusBar } from '../components/app';
 import DocumentView, { RequestedDocumentView}  from '../components/documentView';
 import { DocumentSetView, UploadDocumentsWithDocumentSetId } from '../components/uploadDocuments';
-import SelectRecipients from '../components/selectRecipients';
-import Landing from '../components/landing';
-import Help from '../components/help';
 import { CompletedDocumentSets, PendingDocumentSets, AllDocumentSets } from '../components/documentSets';
 import RequestedSignatures from '../components/requestedSignatures';
 import NotFound from '../components/notFound';
@@ -19,7 +16,6 @@ export default () => {
             <Route path='sign/:documentSetId/:documentId' component={ RequestedDocumentView } />
             <Route component={ContainerWithStatusBar}>
                 <IndexRoute component={UploadDocumentsWithDocumentSetId} />
-                <Route path='faq' component={ Help } />
                 <Route component={Documents}>
                     <Route path='all' component={ AllDocumentSets } />
                     <Route path='to_sign' component={ RequestedSignatures } />

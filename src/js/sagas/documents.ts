a3754257e141741ff1071754dd14265ae168dc61
Paddy Moran <pdmoran@outlook.com>
2017-09-07 14:34:12 +1200

    Final touches on revoke

--- src/js/sagas/documents.ts
+++ src/js/sagas/documents.ts
@@ -1,13 +1,16 @@
-import { all, takeEvery, put, call, select } from 'redux-saga/effects';
+import { all, takeEvery, put, call } from 'redux-saga/effects';
 import axios from 'axios';
-import { closeModal } from '../actions';
+import { closeModal, resetDocuments } from '../actions';
 
 function *revokeSignInvitationSaga() {
     yield takeEvery(Sign.Actions.Types.REVOKE_SIGN_INVITATION, revokeSignInvitation);
 
     function *revokeSignInvitation(action: Sign.Actions.RevokeSignInvitation) {
         yield call(axios.delete, `/api/request_signatures/${action.payload.signRequestId}`);
-        yield put(closeModal({ modalName: Sign.ModalType.CONFIRM_ACTION }));
+        yield all([
+            put(resetDocuments()),
+            put(closeModal({ modalName: Sign.ModalType.CONFIRM_ACTION }))
+        ]);
     }
 }
 

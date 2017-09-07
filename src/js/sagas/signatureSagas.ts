231e2d57b888c8b76b0bb56264e86f94815b7f25
Paddy Moran <pdmoran@outlook.com>
2017-09-08 11:15:04 +1200

    signatures sagas try catches

--- src/js/sagas/signatureSagas.ts
+++ src/js/sagas/signatureSagas.ts
@@ -1,7 +1,7 @@
 import { takeEvery, takeLatest, put, call, all, select } from 'redux-saga/effects';
 import * as Axios from 'axios';
 import axios from 'axios';
-import { selectSignature, setSignatureIds, closeModal, selectInitial } from '../actions/index'
+import { selectSignature, setSignatureIds, closeModal, selectInitial, showFailureModal } from '../actions/index'
 
 interface SignaturesUploadResponse extends Axios.AxiosResponse {
     data: {
@@ -20,14 +20,19 @@ function *uploadSignature() {
     yield takeEvery(Sign.Actions.Types.UPLOAD_SIGNATURE, task);
 
     function *task(action: Sign.Actions.UploadSignature) {
-        const response : SignaturesUploadResponse = yield axios.post('/api/signatures/upload', { base64Image: action.payload.data, type: action.payload.type });
-        let signatureId = response.data.signature_id;
+        try {
+            const response : SignaturesUploadResponse = yield axios.post('/api/signatures/upload', { base64Image: action.payload.data, type: action.payload.type });
+            let signatureId = response.data.signature_id;
 
-        yield put(closeModal({ modalName: 'selectSignature' }));
+            yield put(closeModal({ modalName: 'selectSignature' }));
 
-        // Select the signature or initial
-        let selectAction = action.payload.type === Sign.SignatureType.SIGNATURE ? selectSignature(signatureId) : selectInitial({ initialId: signatureId });
-        yield put(selectAction);
+            // Select the signature or initial
+            let selectAction = action.payload.type === Sign.SignatureType.SIGNATURE ? selectSignature(signatureId) : selectInitial({ initialId: signatureId });
+            yield put(selectAction);
+        }
+        catch (e) {
+            yield put(showFailureModal({ title: 'Upload Signature Failed', message: 'Upload signature failed. Please try again.' }));
+        }
     }
 }
 
@@ -35,36 +40,46 @@ function *deleteSignature() {
     yield takeEvery(Sign.Actions.Types.DELETE_SIGNATURE, task);
 
     function *task(action: Sign.Actions.DeleteSignature) {
-        yield axios.delete(`/api/signatures/${action.payload.signatureId}`);
+        try {
+            yield axios.delete(`/api/signatures/${action.payload.signatureId}`);
+        }
+        catch(e) {
+            yield put(showFailureModal({ title: 'Delete Signature Failed', message: 'Delete signature failed. Please try again.' }));
+        }
     }
 }
 
 function *requestSignatures(action: Sign.Actions.RequestSignatures) {
-    const status = yield select((state: Sign.State) => state.signatures.status);
-    if(status === Sign.DownloadStatus.InProgress){
-        return;
-    }
-    yield put(setSignatureIds({
-        signatureIds: [],
-        initialIds: [],
-        status: Sign.DownloadStatus.InProgress
-    }));
-    const response: SignaturesResponse = yield call(axios.get, '/api/signatures');
-    const signatureIds = response.data.filter(d => d.type === 'signature').map((signature) => signature.signature_id);
-    const initialIds = response.data.filter(d => d.type === 'initial').map((signature) => signature.signature_id);
+    try {
+        const status = yield select((state: Sign.State) => state.signatures.status);
+        if(status === Sign.DownloadStatus.InProgress){
+            return;
+        }
+        yield put(setSignatureIds({
+            signatureIds: [],
+            initialIds: [],
+            status: Sign.DownloadStatus.InProgress
+        }));
+        const response: SignaturesResponse = yield call(axios.get, '/api/signatures');
+        const signatureIds = response.data.filter(d => d.type === 'signature').map((signature) => signature.signature_id);
+        const initialIds = response.data.filter(d => d.type === 'initial').map((signature) => signature.signature_id);
 
-    yield put(setSignatureIds({
-        signatureIds,
-        initialIds,
-        status: Sign.DownloadStatus.Complete
-    }));
-    const selectedSignature = yield select((state: Sign.State) => state.documentViewer.selectedSignatureId);
-    if(signatureIds.length && !selectedSignature) {
-        yield put(selectSignature(signatureIds[0]));
+        yield put(setSignatureIds({
+            signatureIds,
+            initialIds,
+            status: Sign.DownloadStatus.Complete
+        }));
+        const selectedSignature = yield select((state: Sign.State) => state.documentViewer.selectedSignatureId);
+        if(signatureIds.length && !selectedSignature) {
+            yield put(selectSignature(signatureIds[0]));
+        }
+        const selectedInitials = yield select((state: Sign.State) => state.documentViewer.selectedInitialId);
+        if(initialIds.length && !selectedInitials) {
+            yield put(selectInitial({initialId: initialIds[0]}));
+        }
     }
-    const selectedInitials = yield select((state: Sign.State) => state.documentViewer.selectedInitialId);
-    if(initialIds.length && !selectedInitials) {
-        yield put(selectInitial({initialId: initialIds[0]}));
+    catch (e) {
+        //swallow
     }
 }
 
